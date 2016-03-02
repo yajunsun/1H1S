96ae7c7bb5bdd0c334eb967b83f7a0f30bda9509
yajunsun <407250568@qq.com>
2016-03-02 18:08:32 +0800

    20160302注册

--- src/main/java/zgan/ohos/Activities/BindDevice.java
+++ src/main/java/zgan/ohos/Activities/BindDevice.java
@@ -18,6 +18,8 @@ public class BindDevice extends myBaseActivity {
 
     TextInputLayout til_input;
     EditText et_input;
+    String SID;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,40 +29,51 @@ public class BindDevice extends myBaseActivity {
     @Override
     protected void initView() {
         setContentView(R.layout.activity_bind_device);
-        til_input=(TextInputLayout)findViewById(R.id.til_input);
-        et_input=(EditText)findViewById(R.id.et_input);
+        til_input = (TextInputLayout) findViewById(R.id.til_input);
+        et_input = (EditText) findViewById(R.id.et_input);
     }
 
     @Override
     public void ViewClick(View v) {
-        switch (v.getId())
-        {
+        switch (v.getId()) {
             case R.id.btn_bind:
-                String SID=et_input.getText().toString().trim();
-                System.out.print(String.format("%s\t%s\t%s",PreferenceUtil.getCommunityId(), PreferenceUtil.getUserName(),SID));
-                ZganLoginService.toGetServerData(23,254,String.format("%s\t%s\t%s",PreferenceUtil.getCommunityId(), PreferenceUtil.getUserName(),SID),handler);
+                SID = et_input.getText().toString().trim();
+                if (SID.equals("")) {
+                    til_input.setError("SID不能为空");
+                    til_input.setErrorEnabled(true);
+                } else if (SID.length() > 40) {
+                    til_input.setError("SID不正确");
+                    til_input.setErrorEnabled(true);
+                } else {
+                    til_input.setErrorEnabled(false);
+                    System.out.print(String.format("%s\t%s\t%s", PreferenceUtil.getCommunityId(), PreferenceUtil.getUserName(), SID));
+                    ZganLoginService.toGetServerData(23, 254, String.format("%s\t%s\t%s", PreferenceUtil.getCommunityId(), PreferenceUtil.getUserName(), SID), handler);
+                }
                 break;
         }
 
     }
-    Handler handler=new Handler(){
+
+    Handler handler = new Handler() {
         @Override
         public void handleMessage(Message msg) {
             super.handleMessage(msg);
-            if (msg.what==1)
-            {
-                Frame f=(Frame)msg.obj;
-                if (f.subCmd==23)
-                {
-                    String result= generalhelper.getSocketeStringResult(f.strData);
+            if (msg.what == 1) {
+                Frame f = (Frame) msg.obj;
+                if (f.subCmd == 23) {
+                    String result = generalhelper.getSocketeStringResult(f.strData);
                     System.out.print(f.strData);
-                    if (result.equals("0"))
-                    {
-                        generalhelper.ToastShow(BindDevice.this,"绑定室内机成功");
-                    }
-                    else
-                    {
-                        generalhelper.ToastShow(BindDevice.this,"绑定室内机失败");
+                    if (result.equals("0")) {
+                        generalhelper.ToastShow(BindDevice.this, "绑定室内机成功");
+                        PreferenceUtil.setCommunityId(SID);
+                        postDelayed(new Runnable() {
+                            @Override
+                            public void run() {
+                                finish();
+                            }
+                        }, 1000);
+                    } else {
+                        generalhelper.ToastShow(BindDevice.this, "绑定室内机失败");
                     }
                 }
             }
